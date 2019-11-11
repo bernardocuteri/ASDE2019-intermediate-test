59c22e135073047c49eb27d122c2837f1b3ac588
Bernardo Cuteri <bernardo.cuteri@gmail.com>
2019-11-11 12:01:27 +0100

    Basecode for the test

---
+++ weather-forecast-service/src/main/java/it/unical/demacs/asde2019/weatherforecastservice/WeatherForecastServiceApplication.java
@@ -0,0 +1,13 @@
+package it.unical.demacs.asde2019.weatherforecastservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherForecastServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherForecastServiceApplication.class, args);
+	}
+
+}
