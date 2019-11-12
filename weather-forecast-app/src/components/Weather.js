690aadf2c4d185618e09a15648994d6c609dd83a
Alessandro <desim.alex@gmail.com>
2019-11-12 13:42:49 +0100

    Try bonus 2

--- weather-forecast-app/src/components/Weather.js
+++ weather-forecast-app/src/components/Weather.js
@@ -22,17 +22,15 @@ export default class Weather extends React.Component
             let loc = e.target.value;
             AxiosInstance.post("submitLocation", {location: loc})
             .then((res) => {
+                console.log(res.data);
                 this.setState({
                     forecast: res.data,
                     location : loc
                 })
             });
 
-            Axios.get('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22')
-            .then( (res) => {
-                this.setState({
-                    weather : res.data
-                });
+            Axios.post('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22')
+            .catch( (res) => {
                 console.log(res.data);
             })
         }
@@ -49,17 +47,17 @@ export default class Weather extends React.Component
                             onKeyPress={(event) => this.handleSubmit(event)}
                             />
                     </div>
-                    
                 </Grid>
-                    {this.state.forecast.map( (fore, index) => 
-                        <Grid key={index} container justify = "center">
-                        <div>
-                            {index == 0 ? <h4>METEO OGGI</h4> : index == 1 ? <h4>METEO DOMANI</h4> : <h4>METEO DOPODOMANI</h4>}
-                            <h5>Temperatura: {this.state.forecast[index].temp}°C</h5>
-                            <img src={"/static/"+this.state.forecast[index].weather+".png"} alt="- 0 -" width="20%" />
-                        </div>
-                        </Grid>
-                    )}
+
+                {this.state.forecast.map( (fore, index) => 
+                    <Grid key={index} container justify = "center">
+                    <div>
+                        {index == 0 ? <h4>METEO OGGI</h4> : index == 1 ? <h4>METEO DOMANI</h4> : <h4>METEO DOPODOMANI</h4>}
+                        <h5>Temperatura: {this.state.forecast[index].temp}°C</h5>
+                        <img src={"/static/"+this.state.forecast[index].weather+".png"} alt="- 0 -" width="20%" />
+                    </div>
+                    </Grid>
+                )}
             </div>
         )
     }
